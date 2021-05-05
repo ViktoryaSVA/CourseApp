446be5095efb0ee2b06d38493f52273f44836710
vik <vik@localhost.localdomain>
2021-05-05 13:25:01 -0400

    Page for auth

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
 const coursesRoutes = require('./routes/courses')
 const ordersRoutes = require('./routes/orders')
+const authRoutes = require('./routes/auth')
 const Handlebars = require('handlebars')
 const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
 const md5 = require('md5')
@@ -40,6 +41,7 @@ app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
 app.use('/orders', ordersRoutes)
+app.use('/auth', authRoutes)
 
 const PORT = process.env.PORT || 3000
 
